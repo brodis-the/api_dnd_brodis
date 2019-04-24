bf287b1ae0cb6933ca7f2a8e202e0b30e53724da
Abdias Viana <abdviana@gmail.com>
2019-04-24 11:06:25 -0300

    Adding a new project

---
+++ routes.js
@@ -0,0 +1,24 @@
+module.exports = function() {
+
+    return {
+        
+    //classes routes
+    classes: require('./json_files/class/'),
+    artificer: require('./json_files/class/artificer.json'),
+    barbarian: require('./json_files/class/barbarian.json'),
+    bard: require('./json_files/class/bard.json'),
+    cleric: require('./json_files/class/cleric.json'),
+    druid: require('./json_files/class/druid.json'),
+    fighter: require('./json_files/class/fighter.json'),
+    monk: require('./json_files/class/monk.json'),
+    mystic: require('./json_files/class/mystic.json'),
+    paladin: require('./json_files/class/paladin.json'),
+    ranger: require('./json_files/class/ranger.json'),
+    rogue: require('./json_files/class/rogue.json'),
+    sidekick: require('./json_files/class/sidekick.json'),
+    sorcerer: require('./json_files/class/sorcerer.json'),
+    warlock: require('./json_files/class/warlock.json'),
+    wizard: require('./json_files/class/wizard.json'),
+
+    }
+}   
